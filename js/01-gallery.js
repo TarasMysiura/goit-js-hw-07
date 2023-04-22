fd6a7da7e668988011a2e57db25bc9c186ab9803
TarasMysiura <tarasmysiura@gmail.com>
2023-04-22 20:18:42 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,10 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+import * as basicLightbox from "basiclightbox";
 
 // console.log(galleryItems);
 
 const listImg = document.querySelector(".gallery");
-
-// const listContent = galleryItems
-//   .map(
-//     (image) =>
-//       `<li class="gallery__item"><a class="gallery__link" href="large-image.jpg"><img src="${image.preview}"></a></li>`
-//   )
-//   .join("");
-
-// listImg.insertAdjacentHTML("afterbegin", listContent);
 
 const fragment = document.createDocumentFragment();
 
@@ -34,3 +26,136 @@ galleryItems.forEach(function (galleryItem) {
 
 listImg.append(fragment);
 
+listImg.addEventListener("click", (event) => {
+  event.target.src = event.target.dataset.source;
+  console.dir(event.target);
+  const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`);
+  instance.show();
+});
+
+/*
+1. Створити модальне вікно яке буде відкриватися при кліку на кнопку всередині тега body Модальне вікно має закриватися при кліку на напівпрозорий оверлей, та на іконку хрестика всередині модалки
+2. Додати в модалку логіку закриття при натисканні на кнопку **Escape**
+*/
+
+/*
+    1. Отримуємо всі рефси
+    2. Вішаємо обробник подій на кнопку - click
+        2.1. Додаємо клас open до body
+    3. Вішаємо обробник подій на оверлей і на крестик - click
+        3.1. Забираємо клас open у body
+*/
+
+//! 1 варіант - викликати ф-цію toggleModalOpen тільки тоді, коли модалка відкрита
+
+// const refs = {
+//   overlay: document.getElementById("overlay"),
+//   modalCloseBtn: document.getElementById("modalCloseBtn"),
+//   openModalBtn: document.getElementById("openModalBtn"),
+// };
+
+// const openClass = "open";
+
+// refs.openModalBtn.addEventListener("click", toggleModalOpen);
+// refs.overlay.addEventListener("click", toggleModalOpen);
+// refs.modalCloseBtn.addEventListener("click", toggleModalOpen);
+// window.addEventListener("keydown", closeModalOnESC);
+
+// function toggleModalOpen() {
+//   document.body.classList.toggle(openClass);
+// }
+
+// function closeModalOnESC(event) {
+//   if (event.code === "Escape" && document.body.classList.contains(openClass)) {
+//     toggleModalOpen();
+//     console.log("Натиснули Escape");
+//   }
+// }
+
+//! 2 варіант - вручну прибрати клас
+
+// const refs = {
+//   overlay: document.getElementById("overlay"),
+//   modalCloseBtn: document.getElementById("modalCloseBtn"),
+//   openModalBtn: document.getElementById("openModalBtn"),
+// };
+
+// const openClass = "open";
+
+// refs.openModalBtn.addEventListener("click", toggleModalOpen);
+// refs.overlay.addEventListener("click", toggleModalOpen);
+// refs.modalCloseBtn.addEventListener("click", toggleModalOpen);
+// window.addEventListener("keydown", closeModalOnESC);
+
+// function toggleModalOpen() {
+//   document.body.classList.toggle(openClass);
+// }
+
+// function closeModalOnESC(event) {
+//   if (event.code === "Escape") {
+//     document.body.classList.remove(openClass);
+//     console.log("Натиснули Escape");
+//   }
+// }
+
+//! 3 варіант - розділити логіку і створити окремо ф-цію для відкриття і окремо для закриття
+
+// const refs = {
+//   overlay: document.getElementById("overlay"),
+//   modalCloseBtn: document.getElementById("modalCloseBtn"),
+//   openModalBtn: document.getElementById("openModalBtn"),
+// };
+
+// const openClass = "open";
+
+// refs.openModalBtn.addEventListener("click", modalOpen);
+// refs.overlay.addEventListener("click", modalClose);
+// refs.modalCloseBtn.addEventListener("click", modalClose);
+// window.addEventListener("keydown", closeModalOnESC);
+
+// function modalOpen() {
+//   document.body.classList.add(openClass);
+// }
+// function modalClose() {
+//   document.body.classList.remove(openClass);
+// }
+
+// function closeModalOnESC(event) {
+//   if (event.code === "Escape") {
+//     modalClose();
+//     console.log("Натиснули Escape");
+//   }
+// }
+
+//! ====================
+
+// const refs = {
+//   overlay: document.getElementById("overlay"),
+//   modalCloseBtn: document.getElementById("modalCloseBtn"),
+//   openModalBtn: document.getElementById("openModalBtn"),
+// };
+
+// const openClass = "open";
+
+// refs.openModalBtn.addEventListener("click", modalOpen);
+// refs.overlay.addEventListener("click", modalClose);
+// refs.modalCloseBtn.addEventListener("click", modalClose);
+
+// function modalOpen() {
+//   document.body.classList.add(openClass);
+//   window.addEventListener("keydown", closeModalOnESC);
+// }
+
+// function modalClose() {
+//   document.body.classList.remove(openClass);
+//   window.removeEventListener("keydown", closeModalOnESC);
+// }
+
+// function closeModalOnESC(event) {
+//   if (event.code === "Escape") {
+//     modalClose();
+//     console.log("Натиснули Escape");
+//   }
+// }
