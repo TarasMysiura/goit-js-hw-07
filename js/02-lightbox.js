fb4d208a55739c6ea9b86d629d07d60ae776d94f
TarasMysiura <tarasmysiura@gmail.com>
2023-04-23 21:43:05 +0300

    add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const listImg = document.querySelector(".gallery");
+
+const fragment = document.createDocumentFragment();
+
+galleryItems.forEach(function (galleryItem) {
+  const liEl = document.createElement("li");
+  liEl.className = "gallery__item";
+  const aEl = document.createElement("a");
+  aEl.className = "gallery__link";
+  aEl.href = galleryItem.original;
+  const imgEl = document.createElement("img");
+  imgEl.className = "gallery__image";
+  imgEl.src = galleryItem.preview;
+  imgEl.alt = galleryItem.description;
+  aEl.appendChild(imgEl);
+  liEl.appendChild(aEl);
+  fragment.appendChild(liEl);
+});
+
+listImg.append(fragment);
+
+const galleryLightBox = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
